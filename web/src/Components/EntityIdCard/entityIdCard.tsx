1cebc97bbfd3f0d624c48ca17745efc5dde162bc
MewanXeptagon <mewanp@xeptagon.com>
2025-08-25 15:02:13 +0530

    bahrain initial commit/ theme color change done

--- web/src/Components/EntityIdCard/entityIdCard.tsx
+++ web/src/Components/EntityIdCard/entityIdCard.tsx
@@ -18,7 +18,7 @@ const EntityIdCard: React.FC<Props> = ({ calledIn, entId, isValidated }) => {
           placement="right"
         >
           <Tag
-            icon={isValidated ? <CheckCircleOutlined style={{ color: '#8A1538' }} /> : undefined}
+            icon={isValidated ? <CheckCircleOutlined style={{ color: '#CE1126' }} /> : undefined}
             className="entity-id-card"
           >{`${calledIn} ID: ${entId}`}</Tag>
         </Tooltip>
---
+++ web/src/Components/Homepage/DataChart.tsx
@@ -0,0 +1,44 @@
+// src/pages/Home.jsx
+import React, { useEffect, useState } from 'react';
+import LineChart from '../Charts/line chart/lineChart';
+
+interface SeriesType {
+  name: string;
+  data: number[];
+}
+
+const DataLineChart = () => {
+  const [labels, setLabels] = useState<string[]>([]);
+  const [series, setSeries] = useState<SeriesType[]>([]);
+
+  useEffect(() => {
+    fetch('/api/stats/actual?type=ACTIVITY_EMISSIONS')
+      .then((res) => res.json())
+      .then((data) => {
+        const reductionData = data.reductionData;
+
+        const firstSector = Object.keys(reductionData)[0];
+        const years = reductionData[firstSector].map((_: number, idx: number) =>
+          (2000 + idx).toString()
+        );
+
+        const formattedSeries: SeriesType[] = Object.keys(reductionData).map((sector) => ({
+          name: sector,
+          data: reductionData[sector],
+        }));
+
+        setLabels(years);
+        setSeries(formattedSeries);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-xl font-bold mb-4">GHG Actual Reductions</h1>
+      <LineChart labels={labels} series={series} />
+    </div>
+  );
+};
+
+export default DataLineChart;
